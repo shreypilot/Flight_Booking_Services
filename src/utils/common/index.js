f337a715d338087213eeeafe2d21f5cd8f5060c4
shreypilot <shreypilot28@gmail.com>
2024-01-20 12:53:23 +0530

    Added cancel booking logic with cron

--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -2,5 +2,6 @@
 module.exports = {
     ErrorResponse: require('./error-response'),
     SuccessResponse: require('./success-response'),
-    Enums: require('./enums')
+    Enums: require('./enums'),
+        // CRONS: require('./cron-jobs')
 }
